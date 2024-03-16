8d269e393f6d607860219da54416f5452a2fca21
Vedang Patil <vedang1006@gmail.com>
2024-03-16 15:19:00 +1300

    Everything except pawn

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 enum class PieceType
 {
-    None
+    None,
     King,
     Rook,
     Bishop,
@@ -13,9 +14,16 @@ enum class PieceType
     Pawn,
 };
 
+enum class PieceColour
+{
+    None,
+    White,
+    Black
+};
+
 struct Piece
 {
-    bool isWhite;
+    PieceColour colour;
     PieceType type;
 };
 
@@ -28,79 +36,130 @@ private:
 public:
     Board()
     {
-        fill(pieces.begin(), pieces.end(), PieceType.None);
+        fill(pieces, pieces + 64, Piece(PieceColour::None, PieceType::None));
     }
 
-    void generatePseudoLegalMoves(bool colour)
+    void generatePseudoLegalMoves(PieceColour colour)
     {
         moves.clear();
 
         for (int i = 0; i < 64; i++)
         {
-            if (pieces[i] == PieceType.King)
-            {
-                if (i % 8 != 0) moves.emplace_back(i, i - 1);
-                if (i % 8 != 7) moves.emplace_back(i, i + 1);
-                if (i % 8 != 0) moves.emplace_back(i, i - 8);
-                if (i % 8 != 7) moves.emplace_back(i, i + 8);
-            }
-            else if (pieces[i] == PieceType.Rook)
-            {
-                for (int j = i + 1; j < (i / 8 + 1); j++) moves.emplace_back(i, j);
-                for (int j = i - 1; j >= ((i / 8) * 8); j--) moves.emplace_back(i, j);
-                for (int j = i + 8; j < 64; j += 8) moves.emplace_back(i, j);
-                for (int j = i - 8; j >= 0; j -= 8) moves.emplace_back(i, j);
-            }
-            else if (pieces[i] == PieceType.Bishop)
-            {
-                int a = i / 8, b = i % 8;
-                while (a + 1 < 8 && b + 1 < 8) moves.emplace_back(i, (++a) * 8 + (++b));
-                a = i / 8, b = i % 8;
-                while (a > 0 && b > 0) moves.emplace_back(i, (--a) * 8 + (--b));
-                a = i / 8, b = i % 8;
-                while (a + 1 < 8 && b > 0) moves.emplace_back(i, (++a) * 8 + (--b));
-                a = i / 8, b = i % 8;
-                while (a > 0 && b + 1 < 8) moves.emplace_back(i, (--a) * 8 + (++b));
-            }
-            else if (pieces[i] == PieceType.Queen)
-            {
-                for (int j = i + 1; j < (i / 8 + 1); j++) moves.emplace_back(i, j);
-                for (int j = i - 1; j >= ((i / 8) * 8); j--) moves.emplace_back(i, j);
-                for (int j = i + 8; j < 64; j += 8) moves.emplace_back(i, j);
-                for (int j = i - 8; j >= 0; j -= 8) moves.emplace_back(i, j);
+            if (pieces[i].colour != colour) continue;
 
-                int a = i / 8, b = i % 8;
-                while (a + 1 < 8 && b + 1 < 8) moves.emplace_back(i, (++a) * 8 + (++b));
-                a = i / 8, b = i % 8;
-                while (a > 0 && b > 0) moves.emplace_back(i, (--a) * 8 + (--b));
-                a = i / 8, b = i % 8;
-                while (a + 1 < 8 && b > 0) moves.emplace_back(i, (++a) * 8 + (--b));
-                a = i / 8, b = i % 8;
-                while (a > 0 && b + 1 < 8) moves.emplace_back(i, (--a) * 8 + (++b));
+            if (pieces[i].type == PieceType::King)
+            {
+                if (i % 8 != 0 && (colour != pieces[i - 1].colour))
+                {
+                    moves.emplace_back(i, i - 1);
+                }
+                if (i % 8 != 7 && (colour != pieces[i + 1].colour))
+                {
+                    moves.emplace_back(i, i + 1);
+                }
+                if (i % 8 != 0 && (colour != pieces[i - 8].colour))
+                {
+                    moves.emplace_back(i, i - 8);
+                }
+                if (i % 8 != 7 && (colour != pieces[i + 8].colour))
+                {
+                    moves.emplace_back(i, i + 8);
+                }
             }
-            else if (pieces[i] == PieceType.Knight)
+            if (pieces[i].type == PieceType::Rook || pieces[i].type == PieceType::Queen)
+            {
+                for (int j = i + 1; j < (i / 8 + 1); j++)
+                {
+                    if (pieces[j].colour == colour) break;
+                    moves.emplace_back(i, j);
+                    if (pieces[j].colour != PieceColour::None) break;
+                }
+                for (int j = i - 1; j >= ((i / 8) * 8); j--)
+                {
+                    if (pieces[j].colour == colour) break;
+                    moves.emplace_back(i, j);
+                    if (pieces[j].colour != PieceColour::None) break;
+                }
+                for (int j = i + 8; j < 64; j += 8)
+                {
+                    if (pieces[j].colour == colour) break;
+                    moves.emplace_back(i, j);
+                    if (pieces[j].colour != PieceColour::None) break;
+                }
+                for (int j = i - 8; j >= 0; j -= 8)
+                {
+                    if (pieces[j].colour == colour) break;
+                    moves.emplace_back(i, j);
+                    if (pieces[j].colour != PieceColour::None) break;
+                }
+            }
+            if (pieces[i].type == PieceType::Bishop || pieces[i].type == PieceType::Queen)
+            {
+                int a = i / 8, b = i % 8;
+                while (a + 1 < 8 && b + 1 < 8)
+                {
+                    int j = (++a) * 8 + (++b);
+                    if (pieces[j].colour == colour) break;
+                    moves.emplace_back(i, j);
+                    if (pieces[j].colour != PieceColour::None) break;
+                }
+                a = i / 8, b = i % 8;
+                while (a > 0 && b > 0)
+                {
+                    int j = (--a) * 8 + (--b);
+                    if (pieces[j].colour == colour) break;
+                    moves.emplace_back(i, j);
+                    if (pieces[j].colour != PieceColour::None) break;
+                }
+                a = i / 8, b = i % 8;
+                while (a + 1 < 8 && b > 0)
+                {
+                    int j = (++a) * 8 + (--b);
+                    if (pieces[j].colour == colour) break;
+                    moves.emplace_back(i, j);
+                    if (pieces[j].colour != PieceColour::None) break;
+                }
+                a = i / 8, b = i % 8;
+                while (a > 0 && b + 1 < 8)
+                {
+                    int j = (--a) * 8 + (++b);
+                    if (pieces[j].colour == colour) break;
+                    moves.emplace_back(i, j);
+                    if (pieces[j].colour != PieceColour::None) break;
+                }
+            }
+            if (pieces[i].type == PieceType::Knight)
             {
                 int a = i / 8, b = i % 8;
 
                 if (a + 2 < 8)
                 {
-                    if (b + 1 < 8) moves.emplace_back((a + 2) * 8 + b + 1);
-                    if (b - 1 >= 0) moves.emplace_back((a + 2) * 8 + b - 1);
+                    if (b + 1 < 8 && pieces[(a + 2) * 8 + b + 1].colour != colour) moves.emplace_back(i, (a + 2) * 8 + b + 1);
+                    if (b - 1 >= 0 && pieces[(a + 2) * 8 + b - 1].colour != colour) moves.emplace_back(i, (a + 2) * 8 + b - 1);
                 }
                 if (a - 2 >= 0)
                 {
-                    if (b + 1 < 8) moves.emplace_back((a - 2) * 8 + b + 1);
-                    if (b - 1 >= 0) moves.emplace_back((a - 2) * 8 + b - 1);
+                    if (b + 1 < 8 && pieces[(a - 2) * 8 + b + 1].colour != colour) moves.emplace_back(i, (a - 2) * 8 + b + 1);
+                    if (b - 1 >= 0 && pieces[(a - 2) * 8 + b - 1].colour != colour) moves.emplace_back(i, (a - 2) * 8 + b - 1);
                 }
                 if (b + 2 < 8)
                 {
-                    if (a + 1 < 8) moves.emplace_back((a + 1) * 8 + b + 2);
-                    if (a - 1 >= 0) moves.emplace_back((a - 1) * 8 + b + 2);
+                    if (a + 1 < 8 && pieces[(a + 1) * 8 + b + 2].colour != colour) moves.emplace_back(i, (a + 1) * 8 + b + 2);
+                    if (a - 1 >= 0 && pieces[(a - 1) * 8 + b + 2].colour != colour) moves.emplace_back(i, (a - 1) * 8 + b + 2);
                 }
                 if (b - 2 >= 0)
                 {
-                    if (a + 1 < 8) moves.emplace_back((a + 1) * 8 + b - 2);
-                    if (a - 1 >= 0) moves.emplace_back((a - 1) * 8 + b - 2);
+                    if (a + 1 < 8 && pieces[(a + 1) * 8 + b - 2].colour != colour) moves.emplace_back(i, (a + 1) * 8 + b - 2);
+                    if (a - 1 >= 0 && pieces[(a - 1) * 8 + b - 2].colour != colour) moves.emplace_back(i, (a - 1) * 8 + b - 2);
+                }
+            }
+            if (pieces[i].type == PieceType::Pawn)
+            {
+                if (pieces[i].colour == PieceColour::White)
+                {
+                }
+                else
+                {
                 }
             }
         }
